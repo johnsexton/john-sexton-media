18c6393b149b06d1311a72d37d44e143c437edf6
John Sexton <john@sextonmail.com>
2024-11-22 21:52:38 -0500

    major update
    adjusted language in intro, added portrait, added thumbnails to each section, other

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>John Sexton Media</title>
-  <meta name="description" content="John Sexton is a consultant that is available to help you with your instructional design and digital media marketing needs. Service include web design, video production, photography, instructional design, elearning course development, and more!">
+  <title>John Sexton Media | Photo | Video | Web</title>
+  <meta name="description" content="Your one-stop solution for an exceptional photographer, videographer, and web designer in Central Florida servicing the Space Coast (Titusville, Rockledge, Cocoa, Merritt Island, Viera, Melbourne), Orlando, Daytona Beach, and beyond.">
 
   <link rel="stylesheet" href="css/main.css">
 
@@ -19,18 +19,18 @@
   <header id="site-header" role="banner">
     <div class="container">
 
-      <h1 class="site-title"><img class="site-logo" src="img/logo.png" alt="John Sexton Media Logo"> John Sexton <span>Media</span></h1>
+      <h1 class="site-title"><a href="#home"><img class="site-logo" src="img/logo.png" alt="John Sexton Media Logo"> John Sexton <span>Media</span></a></h1>
 
       <input type="checkbox" id="menu-button" />
       <label for="menu-button" class="menu-icon">&#9776;</label>
 
       <nav id="main-menu" role="navigation">
         <ul>
-          <li><a href="#home" class="menu-link">Home</a></li>
-          <li><a href="#instructional-design" class="menu-link">Instructional Design</a></li>
-          <li><a href="#digital-media" class="menu-link">Digital Media</a></li>
-          <li><a href="#resume" class="menu-link">Resume</a></li>
-          <li><a href="#contact" class="menu-link">Contact</a></li>
+          <li><a href="#contact-form" class="menu-link">Contact</a></li>
+          <li><a href="#photo" class="menu-link">Photography</a></li>
+          <li><a href="#video" class="menu-link">Videography</a></li>
+          <li><a href="#web" class="menu-link">Web Design</a></li>
+          <li><a href="#other" class="menu-link">Other</a></li>
         </ul>
       </nav>
 
@@ -38,60 +38,25 @@
   </header>
 
   <main id="site-main">
-
+  
     <section id="home">
-      <div class="container">
-        <h2 class="tagline">Bringing <em>awesome</em> to the table.</h2>
-        <p>Looking for an expert in either instructional design or digital media marketing? Look no further! I come with over a decade of experience, specializing in creating visually stunning content. My skills in instructional design enable me to design and develop <strong>e-learning courses</strong> and other services to help enhance learner performance and productivity. On the digital media front, my skills include <strong>web design</strong>, <strong>videography</strong>, and <strong>photography</strong>. I can help you build your brand by supporting your marketing campaigns to drive results and engage with your target audiences.</p>
-        <p>Currently, I am working full-time for the Division of Academic Innovation @ <a href="https://worldwide.erau.edu/">Embry-Riddle Aeronautical Unviersity Worldwide</a>. However, I am still available for part-time contract work on the side! If you need an expert in either instructional design or digital media, I am your go-to person. Let's connect and discuss how we can work together to achieve your goals.</p>
-      </div>
+      <p class="tagline">Bringing <em>awesome</em> to the table.</p>
     </section>
 
-    <section id="instructional-design">
-      <div class="container">
-        <h2>Instructional Design</h2>
-        <p>As an experienced instructional designer and technologist, I am passionate about creating effective and engaging learning experiences for diverse audiences. With over 10 years of experience, I have designed and developed e-learning courses, instructor-led training materials, and other materials that have helped organizations improve their employee performance and productivity. I use proven instructional design methodologies to ensure that the materials I create are both effective and efficient.</p>
-        <h3 class="sr-only">Instructional Design Services</h3>
-        <ul class="skills">
-          <li><img src="img/icon-instructional-design.png"> <strong>Instructional Design</strong> Effective instructional design ensures that learners achieve their intended outcomes by providing structured and engaging learning experiences.</li>
-          <li><img src="img/icon-elearning.png"> <strong>eLearning Course Development</strong> E-learning course development enables learners to access educational content anytime, anywhere, and at their own pace, making learning more flexible and accessible.</li>
-          <li><img src="img/icon-content-authoring.png"> <strong>Content Authoring</strong> Authoring personalized learning content to your specific needs and requirements, providing a tailored and effective learning experience.</li>
-          <li><img src="img/icon-content-review.png"> <strong>Content Review</strong> Verify that your learning content is accurate, effective, and up-to-date, providing a high-quality and error-free learning experience for your audience.</li>
-          <li><img src="img/icon-video.png"> <strong>Audio &amp; Video Production</strong> Production services enhance the effectiveness of instructional design by creating engaging and interactive multimedia content that helps learners to better understand and retain information.</li>
-          <li><img src="img/icon-ui-ux.png"> <strong>UI/UX and Graphic Design</strong> Creating visually appealing and user-friendly learning materials, which can improve engagement, retention, and overall learning outcomes.</li>
-        </ul>
-      </div>
-    </section>
-
-    <section id="digital-media">
-      <div class="container">
-        <h2>Digital Media</h2>
-        <p>As a digital media expert, I specialize in creating visually compelling content that connects with audiences. My skills include website design and development, video production, and photography. I have worked on a variety of marketing campaigns, helping organizations to tell their stories and engage with their target audiences. With a keen eye for detail and a commitment to quality, I take pride in delivering projects that exceed expectations. Whether you need a website, video, or photos for your marketing campaigns, I can help you achieve your goals. Let's connect and explore how we can collaborate to create stunning content that drives results.</p>
-        <h3 class="sr-only">Digital Media Services</h3>
-        <ul class="skills">
-          <li><img src="img/icon-video-production.png"> <strong>Video Production</strong> Increase brand awareness, engage and retain audiences, and improve conversion rates by creating high-quality and visually compelling video content.</li>
-          <li><img src="img/icon-photography.png"> <strong>Photography</strong> Enhance brand identity, improve engagement, and increase the perceived value of products or services, leading to greater customer trust and loyalty.</li></li>
-          <li><img src="img/icon-live-streaming.png"> <strong>Live Streaming &amp; Event Support</strong> Reach wider audiences, increase engagement and interaction, and create a sense of community and exclusivity around your brand.</li></li>
-          <li><img src="img/icon-web-design.png"> <strong>Web Design & Development</strong> Improve user experience, increase traffic and engagement, and enhance brand credibility and recognition, leading to greater customer acquisition and retention.</li></li>
-          <li><img src="img/icon-graphic-design.png"> <strong>Graphic Design</strong> Create visually appealing and persuasive marketing materials, which can help to build brand awareness, attract new customers, and differentiate your business from competitors.</li>
-          <li><img src="img/icon-ux.png"> <strong>UX/UI</strong> Improve the usability and accessibility of your website, leading to higher customer satisfaction, retention, and advocacy, and ultimately, increased revenue and brand loyalty.</li></li>
-        </ul>
-      </div>
-    </section>
-
-    <section id="resume">
-      <div class="container">
-        <h2>Resume</h2>
-        <p>I bring over a decade of experience working with numerous teams in various industries such as Information Technology, Education, Aeronautics, and Medicine. I have crafted this resume to showcase my skills, experience, and achievements. You'll find that I have a wealth of relevant experience in the industry, along with the education and training necessary to succeed in these fields. From my achievements in previous roles to my skills in communication and leadership, I am confident that I have what it takes to make a positive impact in any organization. So take a few minutes to browse my resume, and see for yourself what sets me apart as a top candidate for any position.</p>
-        <p class="center"><a class="button" href="/docs/JOHN-SEXTON-RESUME.pdf">View My Resume/CV</a></p>
-      </div>
+    <section id="current">
+      <p>In my free time, I'm available for part-time contract work! Let's collaborate and create something amazing together. <br> Otherwise, I currently work full-time for the <a href="https://www.linkedin.com/company/erau-academic-innovation/">Division of Academic Innovation</a> @ <a href="https://worldwide.erau.edu/">Embry-Riddle Aeronautical University Worldwide</a>.</p>
     </section>
 
     <section id="contact">
       <div class="container">
-        <h2>Contact Me</h2>
-        <p>I monitor my inbox from my virtual office regularly. Filling out this form will send me an email with the details you provide. I'll schedule a meeting for us to connect and discuss your project further. I'm very comfortable with all of the common video conferencing software. If you are local and would prefer to meet locally I'm happy to do so. I look foward to our conversation!</p>
-        <form id="contact-form" method="post">
+        <div class="intro col">
+          <img class="circle" src="img/portrait.jpg" alt="John Sexton Portrait">
+          <p>With over a decade of expertise, I specialize in delivering awe-inspiring <strong>photography</strong>, compelling <strong>videography</strong>, and impactful <strong>web design</strong> services throughout Central Florida. Elevate your brand and engage your audience with visually striking content tailored to your unique vision. Let's transform your ideas into captivating realities. Contact me today to discuss how I can bring your story to life!</p>
+        </div>
+        
+        <form id="contact-form" class="col" method="post">
+          <h2>Hit Me Up!</h2>
+          <p class="sm">Filling out this form will send me an email with the details you provide. I typically respond within 24 hours. If you prefer to correspond via text or phone, please provide your preferred contact method in your message.</p>
           <div class="form-group">
             <label for="name">Name</label>
             <input type="text" class="form-control" id="name" name="name" required>
@@ -104,21 +69,130 @@
             <label for="message">Message</label>
             <textarea class="form-control" id="message" name="message" required></textarea>
           </div>
-          <button type="submit" class="btn btn-primary">Send Message</button>
+          <button type="submit">Send Message</button>
         </form>
       </div>
     </section>
 
+    <section id="photo">
+      <div class="container">
+        <div class="col second">
+          <img class="thumb" src="img/thumb-photography.jpg" alt="Photo Camera">
+        </div>
+        <div class="col first">
+          <h2>Photography</h2>
+          <p>I live to capture life's precious moments through a lens.</p>
+          <ul class="tags">
+            <li>Portraits</li>
+            <li>Animals</li>
+            <li>Events</li>
+            <li>Sports</li>
+            <li>Real Estate</li>
+          </ul>
+          <a class="button" href="https://instagram.com/johnsextonmedia/">View My Photography Portfolio <img src="img/social-instagram.png" alt="Instagram Icon"></a>
+        </div>
+<!--
+            <div id="photo-gallery">
+            <img src="img/_DSC2166.jpg">
+            <img src="img/_DSC1099.JPG">
+            <img src="img/_DSC1943.jpeg">
+            <img src="img/_DSC1863.jpg">
+            <img src="img/_DSC2343.jpg">
+            <img src="img/_DSC1130.JPG">
+            <img src="img/_DSC0984.JPG">
+            <img src="img/_DSC2827.jpg">
+            <img src="img/_DSC0637-EDIT.jpg">
+          </div>
+      </div>
+
+      <div id="modal" class="modal">
+        <span class="close" id="close-modal">&times;</span>
+        <img id="modal-image" src="" alt="">
+      </div>
+-->
+        </div>
+      </div>
+    </section>
+
+    <section id="video">
+      <div class="container">
+        <div class="col first">
+          <img class="thumb" src="img/thumb-videography.jpg" alt="Photo Camera">
+        </div>
+        <div class="col second">
+          <h2>Videography</h2>
+          <p>I will produce a visually stunning cinematic film to share your vision.</p>
+          <ul class="tags">
+            <li>Promotional</li>
+            <li>Corporate</li>
+            <li>Live Events</li>
+            <li>Training & Educationa</li>
+            <li>Interviews</li>
+            <li>Real Estate</li>
+          </ul>
+          <a class="button" href="https://youtube.com/@johnsextonmedia/">View My Video Portfolio <img src="img/social-youtube.png" alt="YouTube Icon"></a>
+        </div>
+      </div>
+    </section>
+
+    <section id="web">
+      <div class="container">
+        <div class="col second">
+          <img class="thumb" src="img/thumb-web.jpg" alt="Photo Camera">
+        </div>
+        <div class="col first">
+          <h2>Web Design</h2>
+          <p>You don't need to spend time thinking about your web presence. I'll take care of that for you.</p>
+          <ul class="tags">
+            <li>Business</li>
+            <li>Nonprofit</li>
+            <li>E-commerce</li>
+            <li>Blogs</li>
+            <li>Portfolios</li>
+          </ul>
+          <a class="button" href="https://github.com/johnsexton/">Review My Code <img src="img/social-github.png" alt="Github Icon"></a>
+        </div>
+      </div>
+    </section>
+
+    <section id="other">
+      <div class="container">
+        <div class="col first">
+          <img class="thumb" src="img/thumb-other.jpg" alt="Sunrise in Sweden">
+        </div>
+        <div class="col second">
+          <h2>I Do Other Things Too</h2>
+          <p>I am a man of many trades, although no one calls me Jack. You are welcome to contact me about any of these items. I'm always happy to be helpful. Even if I can't help, I might be able to refer you.</p>
+            <ul class="tags">
+              <li>Instructional Design</li>
+              <li>e-Learning Content Production</li>
+              <li>Graphic Design</li>
+              <li>Computer Builds &amp; Repairs</li>
+              <li>Social Media</li>
+              <li>Project Management</li>
+              <li>Wrike</li>
+              <li>Notion</li>
+              <li>Live Streaming</li>
+              <li>YouTube</li>
+              <li>Twitch</li>
+            </ul>
+          </p>
+        </div>
+      </div>
+    </section>
   </main>
 
   <footer id="site-footer">
     <div class="container">
-      <div class="copyright">&copy; <?php echo date("Y"); ?> John Sexton. All rights reserved.</div>
+      <div class="copyright">&copy; <?php echo date("Y"); ?> John Sexton Media, LLC. All rights reserved.</div>
       <div class="social">
         <h2 class="sr-only">Social Media</h2>
         <ul class="social-links">
           <li><a href="https://linkedin.com/in/johnsexton"><img src="img/social-linkedin.png" alt="Linkedin Icon"></a></li>
           <li><a href="https://instagram.com/johnsextonmedia"><img src="img/social-instagram.png" alt="Instagram Icon"></a></li>
+          <li><a href="https://youtube.com/@johnsextonmedia"><img src="img/social-youtube.png" alt="YouTube Icon"></a></li>
+          <li><a href="https://github.com/johnsexton"><img src="img/social-github.png" alt="Github Icon"></a></li>
+          
         </ul>
       </div>
     </div>
